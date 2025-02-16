f06096b661b55ff12d02797b992fcebf771d7185
Sharad Bista <124153048+SharadHub@users.noreply.github.com>
2025-02-16 21:32:18 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function addTask() {
+    let taskInput = document.getElementById("taskInput");
+    let taskList = document.getElementById("taskList");
+
+    if (taskInput.value.trim() !== "") {
+        let li = document.createElement("li");
+        li.textContent = taskInput.value;
+        taskList.appendChild(li);
+        taskInput.value = "";
+    } else {
+        alert("Please enter a task!");
+    }
+}
